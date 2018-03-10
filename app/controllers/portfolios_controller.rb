bb089a5a07326d6598a5b001e3c8897a5b9cf9bb
Omar Albacha <oalbacha@gmail.com>
2018-03-10 16:53:34 +0400

    Refactor before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
   
 	def index
@@ -70,6 +71,10 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       technologies_attributes: [:name]
                                       )
+  end
+
+  def Set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
