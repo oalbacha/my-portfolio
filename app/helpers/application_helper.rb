a15d45b8a02b90f139a22f9229e37d1f08188925
Omar Albacha <oalbacha@gmail.com>
2018-03-11 15:17:40 +0400

    Integrate my own new custom gem into my portfolio rails application to display copyright on the bottom of all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    OalbachaViewTool::Renderer.copyright 'Omar Albacha', 'All Rights Reserved'
+  end
 end
