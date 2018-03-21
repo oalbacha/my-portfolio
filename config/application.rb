3227f6c54df86e44cfe1e5d4d9d8ca104d1bd7d2
Omar Albacha <oalbacha@gmail.com>
2018-03-21 18:07:04 +0400

    Implement initial connection to the Twitter API

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module MyPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -28,5 +29,6 @@ module MyPortfolio
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
   end
 end
